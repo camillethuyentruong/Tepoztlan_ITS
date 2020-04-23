612c4b562b8f9ae5a320414059c072f7aeba5232
Alicia Mastretta-Yanes <ticatla@gmail.com>
2020-04-23 18:41:50 -0500

    modified trimmomatic script to add example of a loop

--- bin/trimmomatic.sh
+++ bin/trimmomatic.sh
@@ -11,40 +11,21 @@
 mkdir -p ../data/trimfilter
 
 
-# Trimming the Read R1_Forward
+# Cleaning Read R1_Forward
 
+for sample in ###LISTA AQUI todos los R1##; do
+java -jar trimmomatic-0.39.jar SE ../data/tepoz/${sample}.fastq.gz ../data/trimfilter/${sample}_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
+done
 
-java -jar trimmomatic-0.39.jar SE ../data/tepoz/SCC1J_S3_L001_R2_001.fastq.gz ../data/trimfilter/SCC1J_S3_L001_R2_001_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
+# Cleaning the Read R2_Reverse
 
-####Trying to run it with all the SCC* samples. IT DID NOT WORK OUT; SO I DID IT (BY THE STUPID WAY 1 BY 1 :/ ) ???
-
-java -jar trimmomatic-0.39.jar SE ../data/tepoz/SCC*.fastq.gz ../data/trimfilter/SCC*_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
-
-
-# Trimming the Read R2_Reverse
-
-
-java -jar trimmomatic-0.39.jar SE ../data/tepoz/SCC1J_S3_L001_R2_001.fastq.gz ../data/trimfilter/SCC1J_S3_L001_R2_001_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
-
+for sample in ###LISTA AQUI todos los R2##; do
+java -jar trimmomatic-0.39.jar SE ../data/tepoz/${sample}.fastq.gz ../data/trimfilter/${sample}_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
+done
 
 #To perform the fastqc for R1 and R2
 
 fastqc ../data/trimfilter/*.fastq.gz
-
-
-### To Have a security copy scp in my PC
-
-#Create directory for output
-
-mkdir -p preprocessing/trimmedsecs
-
-#Using the scp command for the different samples
-
-scp orojo@nodo7.conabio.gob.mx:/LUSTRE/Genetica/orojo/Tepoztlan_ITS/data/trimfilter/SCH*.*html .
-scp orojo@nodo7.conabio.gob.mx:/LUSTRE/Genetica/orojo/Tepoztlan_ITS/data/trimfilter/SCC*.*html .
-scp orojo@nodo7.conabio.gob.mx:/LUSTRE/Genetica/orojo/Tepoztlan_ITS/data/trimfilter/SD*.*html .
-
-
 
 
 
