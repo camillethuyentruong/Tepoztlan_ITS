33b83e4bc9fef08c1f8356f31b7ca9522a04819f
Alicia Mastretta-Yanes <ticatla@gmail.com>
2020-04-23 19:00:19 -0500

    added path to adapters in trimmomatic script

--- bin/trimmomatic.sh
+++ bin/trimmomatic.sh
@@ -14,13 +14,13 @@ mkdir -p ../data/trimfilter
 # Cleaning Read R1_Forward
 
 for sample in ###LISTA AQUI todos los R1##; do
-java -jar trimmomatic-0.39.jar SE ../data/tepoz/${sample}.fastq.gz ../data/trimfilter/${sample}_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
+java -jar trimmomatic-0.39.jar SE ../data/tepoz/${sample}.fastq.gz ../data/trimfilter/${sample}_trimmed.fastq.gz ILLUMINACLIP:adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
 done
 
 # Cleaning the Read R2_Reverse
 
 for sample in ###LISTA AQUI todos los R2##; do
-java -jar trimmomatic-0.39.jar SE ../data/tepoz/${sample}.fastq.gz ../data/trimfilter/${sample}_trimmed.fastq.gz ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
+java -jar trimmomatic-0.39.jar SE ../data/tepoz/${sample}.fastq.gz ../data/trimfilter/${sample}_trimmed.fastq.gz ILLUMINACLIP:adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:7:20
 done
 
 #To perform the fastqc for R1 and R2
